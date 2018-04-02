2342706e7437ecfc30d65b0c405ecd6d797c8259
LUCY78765580 <384698960@qq.com>
2018-04-02 11:20:15 +0800

    v2.0

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os
 from app import create_app, db
-from app.models import User, Say, Post,Link,Tag
+from app.models import User,Say,Post,Link,Tag,Comment
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
@@ -9,15 +9,32 @@ app=create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager=Manager(app)
 migrate=Migrate(app,db)
 
+@app.shell_context_processor
 def make_shell_context():
-	return dict(app=app, db=db, User=User,Say=Say,Post=Post,Link=Link,Tag=Tag)
+	return dict(app=app, db=db, User=User,Say=Say,Post=Post,Link=Link,Tag=Tag,Comment=Comment)
 
-manager.add_command('shell', Shell(make_context=make_shell_context))
-manager.add_command('db',MigrateCommand)
+def profile(length, profile_dir):
+	"""Start the application under the code profiler."""
+	from werkzeug.contrib.profiler import ProfilerMiddleware
+	app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[length],profile_dir=profile_dir)
+	app.run()
+
+@app.cli.command()
+def deploy():
+	"""Run deployment tasks."""
+	# migrate database to latest revision
+	upgrade()
 
 
-if __name__=='__main__':
-	manager.run()
+
+
+
+#manager.add_command('shell', Shell(make_context=make_shell_context))
+#manager.add_command('db',MigrateCommand)
+
+
+#if __name__=='__main__':
+#	manager.run()
 
 
 
