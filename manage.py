df5a14826697c77186ae1cb02af07fe18ee34615
LUCY78765580 <384698960@qq.com>
2018-04-02 14:17:34 +0800

    v2.0

--- manage.py
+++ manage.py
@@ -9,33 +9,29 @@ app=create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager=Manager(app)
 migrate=Migrate(app,db)
 
-@app.shell_context_processor
+
+#@app.shell_context_processor
 def make_shell_context():
 	return dict(app=app, db=db, User=User,Say=Say,Post=Post,Link=Link,Tag=Tag,Comment=Comment)
 
-def profile(length, profile_dir):
-	"""Start the application under the code profiler."""
-	from werkzeug.contrib.profiler import ProfilerMiddleware
-	app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[length],profile_dir=profile_dir)
-	app.run()
+#def profile(length, profile_dir):
+#	"""Start the application under the code profiler."""
+#	from werkzeug.contrib.profiler import ProfilerMiddleware
+#	app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[length],profile_dir=profile_dir)
+#	app.run()
 
-@app.cli.command()
-def deploy():
-	"""Run deployment tasks."""
-	# migrate database to latest revision
-	upgrade()
-
-
+#@app.cli.command()
+#def deploy():
+#	"""Run deployment tasks."""
+#	# migrate database to latest revision
+#	upgrade()
 
 
-
-#manager.add_command('shell', Shell(make_context=make_shell_context))
-#manager.add_command('db',MigrateCommand)
-
-
-#if __name__=='__main__':
-#	manager.run()
+manager.add_command('shell', Shell(make_context=make_shell_context))
+manager.add_command('db',MigrateCommand)
 
 
+if __name__=='__main__':
+	manager.run()
 
 
