62d0905aa1da54caaca1e28ff17d186315548dcc
LUCY78765580 <384698960@qq.com>
2018-04-03 11:01:04 +0800

    v2.0

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os
+import click
 from app import create_app, db
 from app.models import User,Say,Post,Link,Tag,Comment
 from flask_script import Manager, Shell
@@ -7,11 +8,22 @@ from flask_migrate import Migrate, MigrateCommand
 
 app=create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate=Migrate(app,db)
-manager=Manager(app)
+#manager=Manager(app)
 
 
+@app.cli.command()
 def make_shell_context():
 	return dict(app=app, db=db, User=User,Say=Say,Post=Post,Link=Link,Tag=Tag,Comment=Comment)
+
+
+@app.cli.command()
+@click.option('--length', default=25,help='Number of functions to include in the profiler report.')
+@click.option('--profile-dir', default=None,help='Directory where profiler data files are saved.')
+def profile(length, profile_dir):
+	"""Start the application under the code profiler."""
+	from werkzeug.contrib.profiler import ProfilerMiddleware
+	app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[length],profile_dir=profile_dir)
+	app.run()
 
 
 @app.cli.command()
@@ -29,7 +41,7 @@ def deploy():
 #manager.add_command('shell', Shell(make_context=make_shell_context))
 #manager.add_command('db',MigrateCommand)
 
-if __name__=='__main__':
-	manager.run()
+#if __name__=='__main__':
+#	manager.run()
 
 
