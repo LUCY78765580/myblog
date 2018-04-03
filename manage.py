66476e08df5520e70897f5c2177a33e6928cb0a1
Wakingup <384698960@qq.com>
2018-04-03 19:51:17 +0800

    v2.0

--- manage.py
+++ manage.py
@@ -43,7 +43,7 @@ def deploy():
 #manager.add_command('shell', Shell(make_context=make_shell_context))
 #manager.add_command('db',MigrateCommand)
 
-if __name__=='__main__':
-	manager.run()
+#if __name__=='__main__':
+#	manager.run()
 
 
