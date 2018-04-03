3ea492f4082c0fa5e63ed60b2f48f0f22b7236cf
LUCY78765580 <384698960@qq.com>
2018-04-03 13:12:06 +0800

    v2.0

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import click
 from app import create_app, db
 from app.models import User,Say,Post,Link,Tag,Comment
 from flask_script import Manager, Shell
-from flask_migrate import Migrate, upgrade
+from flask_migrate import Migrate, MigrateCommand,upgrade
 
 app=create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate=Migrate(app,db)
